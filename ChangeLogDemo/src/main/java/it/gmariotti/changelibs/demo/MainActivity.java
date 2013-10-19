500512211aae73b25d777e5df2a209de835d6845
gabrielemariotti <gabri.mariotti@gmail.com>
2013-10-19 10:50:09 +0200

    Prepared release 1.3.0

--- ChangeLogDemo/src/main/java/it/gmariotti/changelibs/demo/MainActivity.java
+++ ChangeLogDemo/src/main/java/it/gmariotti/changelibs/demo/MainActivity.java
@@ -241,7 +241,7 @@ public class MainActivity extends ActionBarActivity {
             if (prev != null) {
                 ft.remove(prev);
             }
-            ft.addToBackStack(null);
+            //ft.addToBackStack(null);
 
             dialogStandardFragment.show(ft,"changelogdemo_dialog");
         }
@@ -253,7 +253,7 @@ public class MainActivity extends ActionBarActivity {
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
             fragmentTransaction.replace(R.id.fragment_main,baseFragment);
-            fragmentTransaction.addToBackStack(null);
+            //fragmentTransaction.addToBackStack(null);
             fragmentTransaction.commit();
             mCurrentTitle=baseFragment.getTitleResourceId();
         }
